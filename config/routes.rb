edd32fa029a6ad29ce1974227ec7a107cd8fcbe9
Supachai <jordekdom@gmail.com>
2017-07-11 09:28:07 +0000

    Add Friendships model

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
-  
+  get 'my_friends', to: "users#my_friends"
   
   #get 'welcome',to: 'welcomes#index'
   # The priority is based upon order of creation: first created -> highest priority.
